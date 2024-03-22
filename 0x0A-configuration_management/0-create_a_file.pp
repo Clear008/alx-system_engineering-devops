aded730dc1656e6e6e1db397c55c740bc1178091
Clear008 <wissal-exo@hotmail.com>
2024-03-22 09:25:11 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,10 @@
-# manifest to create file in tmp directory
+# create a file school in tmp
+
 file { '/tmp/school':
   ensure  => present,
   path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
+  groupe  => 'www-data',
   content => 'I love Puppet',
 }
