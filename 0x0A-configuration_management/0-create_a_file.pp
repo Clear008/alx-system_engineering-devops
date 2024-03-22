54d8cfc8d791e4bc8f2733d84d1ddb0784aff0a7
Clear008 <wissal-exo@hotmail.com>
2024-03-22 09:25:55 -0700

    0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
 # create a file school in tmp
 
 file { '/tmp/school':
-  ensure  => present,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  groupe  => 'www-data',
-  content => 'I love Puppet',
+ensure  => present,
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+groupe  => 'www-data',
+content => 'I love Puppet',
 }
