f964d82642eae26df7b424e12228d31bbc3a9f73
Clear008 <wissal-exo@hotmail.com>
2024-03-23 07:43:18 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,11 @@
 # install flask from pip3.
-
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
+
+package { 'werkzeug':
+  ensure   => '2.0.3', # or the appropriate version compatible with Flask 2.1.0
+  provider => 'pip3',
+}
+
