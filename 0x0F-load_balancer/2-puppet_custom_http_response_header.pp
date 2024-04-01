30551ce51a39a16a8c6b2e0f99ca1106bfbb08b7
Clear008 <wissal-exo@hotmail.com>
2024-04-01 16:08:00 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,22 +1,22 @@
 # automate the task of creating a custom HTTP header response, but with Puppet
 ## Ensure nginx package is installed
-package { 'nginx':
-  ensure => 'installed',
+
+
+exec { 'update_system':
+  command => '/usr/bin/apt-get -y update',
 }
 
-file { '/var/www/html/index.html':
-  ensure  => 'file',
-  content => 'Hello World!',
+exec { 'install_nginx':
+  command => '/usr/bin/apt-get -y install nginx',
+  require => Exec['update_system'],
 }
 
-file_line { 'add_custom_http_header':
-  ensure  => 'present',
-  path    => '/etc/nginx/sites-available/default',
-  line    => 'add_header X-Served-By $hostname;',
-  require => Package['nginx'],
+exec { 'add_custom_http_header':
+  command => 'sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default',
+  require => Exec['install_nginx'],
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+exec { 'restart_nginx':
+  command => 'service nginx restart',
+  require => Exec['add_custom_http_header'],
 }
