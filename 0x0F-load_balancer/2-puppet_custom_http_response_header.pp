9b120d643646d02b1c18ed719b8a02925aa016a3
Clear008 <wissal-exo@hotmail.com>
2024-04-01 16:04:22 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -4,23 +4,18 @@ package { 'nginx':
   ensure => 'installed',
 }
 
-# Create index.html file
 file { '/var/www/html/index.html':
   ensure  => 'file',
   content => 'Hello World!',
 }
 
-exec {'redirect_me':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell'
+file_line { 'add_custom_http_header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
-exec {'HTTP header':
-  command  => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
-  provider => 'shell'
-}
-
-# Ensure nginx service is running and restart if needed
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
