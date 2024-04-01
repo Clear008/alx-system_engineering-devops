1005994ab04ae506fae48c331562d202aadae4bc
Clear008 <wissal-exo@hotmail.com>
2024-04-01 15:32:02 -0700

    2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,9 +1,34 @@
-# automate the task of creating a custom HTTP header response, but with Puppet
+# Ensure nginx package is installed
+package { 'nginx':
+  ensure => 'installed',
+}
 
-exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
-  provider => shell,
+# Create index.html file
+file { '/var/www/html/index.html':
+  ensure  => 'file',
+  content => 'Hello World!',
+}
+
+# Add custom HTTP header to nginx configuration
+file_line { 'add_custom_header':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  line    => 'add_header X-Served-By $hostname;',
+  match   => 'listen 80 default_server;',
+  after   => true,
+  require => Package['nginx'],
+}
+
+# Configure redirection
+file_line { 'configure_redirection':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
+}
+
+# Ensure nginx service is running and restart if needed
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
