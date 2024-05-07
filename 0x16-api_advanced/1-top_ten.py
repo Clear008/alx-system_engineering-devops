6d89a2d8b39cee9867bc0659d7ad428f921ffada
Clear008 <wissal-exo@hotmail.com>
2024-05-07 12:38:41 -0700

    1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""contains the function top_ten"""
+"""Contains the function top_ten"""
 
 import requests
 
-
 def top_ten(subreddit):
-    """returns the top ten posts for a given subreddit"""
+    """Returns the top ten posts for a given subreddit"""
+
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'CustomBot/1.0'}
     rsp = requests.get(url, headers=headers, allow_redirects=False)
-    if rsp.status_code != 200:
+
+    if rsp.status_code == 200:
+        data = rsp.json().get("data", {}).get("children", [])
+        top10_posts = "\n".join(post["data"]["title"] for post in data)
+        print(top10_posts)
+    else:
         print(None)
-        return
-    data = rsp.json().get("data").get("children")
-    top10_posts = "\n".join(post.get("data").get("title") for post in data)
-    print(top10_posts)
